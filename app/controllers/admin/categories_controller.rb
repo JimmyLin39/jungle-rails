2a0c9c1cfe22433d3d75bfb6a89dbc1a259661ff
JimmyLin39 <jimmylin0309@email.com>
2017-12-06 17:06:25 -0800

    modify new and create method

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -3,5 +3,22 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_param)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  def category_param
+    params.require(:category).permit(
+      :name
+    )
   end
 end
