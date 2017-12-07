4407d3dabf38facd0edf047513ca61082d17116e
JimmyLin39 <jimmylin0309@email.com>
2017-12-07 13:46:58 -0800

    add new and create action for create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController
   def index
+    # @users = User.where(user_param.email == User.email, user_param.password == User.password )
+  end
+
+  def show
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_param)
+    
+    if @user.save
+      redirect_to [:products], notice: 'Account created!'
+    else
+      render :new
+    end
+  end
+
+  def user_param
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password
+    )
   end
 end
