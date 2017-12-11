48815757408c8e438a429c4ae782e5c8a802fed3
JimmyLin39 <jimmylin0309@email.com>
2017-12-11 13:37:14 -0800

    add four examples for each validation and one example if has all fields set

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    # validation tests/examples here
+
+    it 'should be valid with all four fields set' do
+      category = Category.new(name: 'new')      
+      product = Product.new(name: 'book', price: 5000, quantity: 5, category: category)
+      expect(product).to be_valid
+    end
+    
+    it 'should not be valid without a name' do
+      category = Category.new(name: 'new') 
+      product = Product.new(price: 5000, quantity: 5, category: category)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Name can\'t be blank')
+    end
+
+    it 'should not be valid without a price' do
+      category = Category.new(name: 'new') 
+      product = Product.new(name: 'book', quantity: 5, category: category)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Price can\'t be blank')
+    end
+
+    it 'should not be valid without a quantity' do
+      category = Category.new(name: 'new') 
+      product = Product.new(name: 'book', price: 5000, category: category)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Quantity can\'t be blank')
+    end
+
+    it 'should not be valid without a category' do
+      product = Product.new(name: 'book', price: 5000, quantity: 5)
+      expect(product).to_not be_valid
+      expect(product.errors.full_messages).to include('Category can\'t be blank')
+    end
+
+  end
 end
